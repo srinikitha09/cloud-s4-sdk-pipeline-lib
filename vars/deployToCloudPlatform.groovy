58a90c33d4b8aeed9bd8f3d64dc7374a3eb48889
Ramachandra Kamath Arbettu <ramachandra.kamath.arbettu@sap.com>
2018-07-26 12:46:30 +0200

    Remove node dependencies for k8s

--- vars/deployToCloudPlatform.groovy
+++ vars/deployToCloudPlatform.groovy
@@ -14,8 +14,7 @@ def call(Map parameters = [:]) {
                     deployments["Deployment ${index > 1 ? index : ''}"] = {
                         deployToCfWithCli script: parameters.script, appName: target.appName, org: target.org, space: target.space, apiEndpoint: target.apiEndpoint, manifest: target.manifest, credentialsId: target.credentialsId, deploymentType: DeploymentType.selectFor(CloudPlatform.CLOUD_FOUNDRY, parameters.isProduction.asBoolean())
                     }
-                }
-             else {
+                } else {
                 deployments["Deployment ${index > 1 ? index : ''}"] = {
                     node(env.NODE_NAME) {
                         unstashFiles script: script, stage: stageName
@@ -34,13 +33,20 @@ def call(Map parameters = [:]) {
             def source = "application/target/${pom.getArtifactId()}.${pom.getPackaging()}"
             for (int i = 0; i < parameters.neoTargets.size(); i++) {
                 def target = parameters.neoTargets[i]
-                deployments["Deployment ${index > 1 ? index : ''}"] = {
-                    node(env.NODE_NAME) {
-                        unstashFiles script: script, stage: stageName
+                if (env.jaas_owner) {
+                    deployments["Deployment ${index > 1 ? index : ''}"] = {
                         deployToNeoWithCli script: parameters.script, target: target, deploymentType: DeploymentType.selectFor(CloudPlatform.NEO, parameters.isProduction.asBoolean()), source: source
-                        stashFiles script: script, stage: stageName
+                    }
+                } else {
+                    deployments["Deployment ${index > 1 ? index : ''}"] = {
+                        node(env.NODE_NAME) {
+                            unstashFiles script: script, stage: stageName
+                            deployToNeoWithCli script: parameters.script, target: target, deploymentType: DeploymentType.selectFor(CloudPlatform.NEO, parameters.isProduction.asBoolean()), source: source
+                            stashFiles script: script, stage: stageName
+                        }
                     }
                 }
+
                 index++
             }
             runClosures deployments, script
