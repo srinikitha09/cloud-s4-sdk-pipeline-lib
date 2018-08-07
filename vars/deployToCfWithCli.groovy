b5bf4d36410e4b33b8588bc1f37d847797897760
Ramachandra Kamath Arbettu <ramachandra.kamath.arbettu@sap.com>
2018-08-07 16:40:56 +0200

    Check home dir

--- vars/deployToCfWithCli.groovy
+++ vars/deployToCfWithCli.groovy
@@ -60,7 +60,7 @@ def call(Map parameters = [:]) {
 
 private deploy(script, dockerImage, deploymentType, cfTarget, statusCode) {
     dockerExecute(script: script, dockerImage: dockerImage) {
-        sh "pwd && sleep 600"
+        sh "pwd && ~ && sleep 600"
         lock("${cfTarget.apiEndpoint}/${cfTarget.org}/${cfTarget.space}/${cfTarget.appName}") {
             if (deploymentType == DeploymentType.BLUE_GREEN) {
                 withEnv(["STATUS_CODE=${statusCode}"]) {
