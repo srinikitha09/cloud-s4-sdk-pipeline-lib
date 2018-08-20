51f5d9a384a6d29c26874919a93e58fc66a629fc
Ramachandra Kamath Arbettu <ramachandra.kamath.arbettu@sap.com>
2018-08-20 14:20:38 +0200

    Init config after setArtifact version

--- vars/stageInitS4sdkPipeline.groovy
+++ vars/stageInitS4sdkPipeline.groovy
@@ -17,9 +17,7 @@ def call(Map parameters) {
 
         initS4SdkPipelineLibrary script: script
         initStashConfiguration script: script
-        if (Boolean.valueOf(env.ON_K8S)) {
-            initContainersMap script: script
-        }
+
 
         def mavenLocalRepository = new File(script.s4SdkGlobals.m2Directory)
         def reportsDirectory = new File(script.s4SdkGlobals.reportsDirectory)
@@ -53,6 +51,10 @@ def call(Map parameters) {
         generalConfiguration.gitCommitId = getGitCommitId()
 
         String prefix = generalConfiguration.projectName
+
+        if (Boolean.valueOf(env.ON_K8S)) {
+            initContainersMap script: script
+        }
 
         script.commonPipelineEnvironment.configuration.currentBuildResultLock = "${prefix}/currentBuildResult"
         script.commonPipelineEnvironment.configuration.performanceTestLock = "${prefix}/performanceTest"
