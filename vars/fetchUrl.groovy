9b41a582eb3e16ecc87ca6c1080e7af87ca6bf14
Florian Wilhelm <florian.wilhelm02@sap.com>
2019-07-15 19:25:02 +0200

    update

--- vars/fetchUrl.groovy
+++ vars/fetchUrl.groovy
@@ -3,6 +3,6 @@ import com.cloudbees.groovy.cps.NonCPS
 @NonCPS
 def call(url) {
     System.setProperty("http.proxyHost", Jenkins.instance.proxy.name);
-    System.setProperty("http.proxyPort", Jenkins.instance.proxy.port);
+    System.setProperty("http.proxyPort", "${Jenkins.instance.proxy.port}");
     return new URL(url).getText()
 }
